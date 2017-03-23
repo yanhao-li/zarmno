66ed2baa567166e789353d613805b20af959a2f7
Roy <Roy@compscis-iMac-4.local>
2017-03-23 14:42:20 -0400

    add Homepage Jumbotron

---
+++ app/components/HomePageJumbotron.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+
+export default class HomePageJumbotron extends React.Component {
+    render(){
+        return(
+            <Jumbotron>
+
+            </Jumbotron>
+        );
+    }
+
+}
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,11 +10,12 @@
  */
 
 import React from 'react';
+import Jumbotron from 'components/HomePageJumbotron'
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-        <h1>This is HomePage</h1>
+        <Jumbotron />
     );
   }
 }
