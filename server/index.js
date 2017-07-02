8aa80fd6a7cc4a9960e1a85107e866cb10893f70
Yanhao Li <hi@yanhaoli.com>
2017-07-01 21:53:32 -0400

    remove remote api

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
 
-app.use('/v1', require('./api/app'));
+app.use('/api/v1', require('./api/app'));
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
