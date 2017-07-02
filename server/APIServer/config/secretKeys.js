70790b1b4ee21865e68a4d649d3b0c5f4cb309b2
Yanhao Li <hi@yanhaoli.com>
2017-07-02 11:17:30 -0400

    separate the api server from project

--- server/APIServer/config/secretKeys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: 'menuplussecrectkey',
-};
--- server/index.js
+++ server/index.js
@@ -1,49 +1,19 @@
-/* eslint consistent-return:0 */
-
 const express = require('express');
 const logger = require('./logger');
-const argv = require('minimist')(process.argv.slice(2));
-const setup = require('./middlewares/frontendMiddleware');
-const isDev = process.env.NODE_ENV !== 'production';
-const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
-const resolve = require('path').resolve;
+const bodyParser = require('body-parser');
+const models = require('./models');
+const allowCrossDomain = require('./middlewares/allowCrossDomain');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(allowCrossDomain);
+app.use('/v1', require('./api/app'));
 
-// If you need a backend, e.g. an API, add your custom backend-specific middleware here
-// app.use('/api', myApi);
+models.sequelize.sync();
 
-// In production we need to pass these values in instead of relying on webpack
-setup(app, {
-  outputPath: resolve(process.cwd(), 'build'),
-  publicPath: '/',
-});
-
-
-// get the intended host and port number, use localhost and port 3000 if not provided
-const customHost = argv.host || process.env.HOST;
-const host = customHost || null; // Let http.Server use its default IPv6/4 host
-const prettyHost = customHost || 'localhost';
-
-const port = argv.port || process.env.PORT || 3000;
-
-// Start your app.
-
-app.listen(port, host, (err) => {
+app.listen(8080, (err) => {
   if (err) {
     return logger.error(err.message);
   }
-
-  // Connect to ngrok in dev mode
-  if (ngrok) {
-    ngrok.connect(port, (innerErr, url) => {
-      if (innerErr) {
-        return logger.error(innerErr);
-      }
-
-      logger.appStarted(port, prettyHost, url);
-    });
-  } else {
-    logger.appStarted(port, prettyHost);
-  }
+  logger.appStarted(8080, 'localhost');
 });
